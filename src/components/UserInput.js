75871ebbc1d7b4f7898fc19e703643ea9e86f9bb
Nabzcreates <88214757+Nabzcreates@users.noreply.github.com>
2022-01-16 00:39:40 -0800

    adding

--- src/components/UserInput.js
+++ src/components/UserInput.js
@@ -15,7 +15,7 @@ function UserInput({userRef, dispatch}) {
         <h4>devfinder</h4>
         <div className="darkMode" >
           <p id="modeText">DARK</p>
-          <img src="./assets/icon-moon.svg" id = "dark_btn"alt=""/>
+          <img  src="./assets/icon-moon.svg" id = "dark_btn"alt=""/>
         </div>        
       </div>
 
